d0951ea62cbb0dfaf4714533960d357e69f92e3e
alirezahabibi88 <alirezahbibi209@gmail.com>
2026-01-05 05:45:44 +0330

    Add files via upload

---
+++ website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse,JsonResponse
+
+def index_view(request):
+  return render(request,'website/index.html')
+
+def about_view(request):
+    return render(request,'website/about.html')
+
+def contact_view(request):
+    return render(request,'website/contact.html')
+
+def test_view(request):
+    context = {'name':'Alireza', 'last_name':'Habibi'}
+    return render(request,'website/test.html',context)
